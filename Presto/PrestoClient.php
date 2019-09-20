66ebc8176d8d5786b756c792d2e6d1cf6e4722e4
dannyhu926 <469671292@qq.com>
2019-09-20 16:48:56 +0800

    Update PrestoClient.php
    多条sql查询bug

--- Presto/PrestoClient.php
+++ Presto/PrestoClient.php
@@ -66,7 +66,8 @@ class PrestoClient
                 $response[$key][$this->queryColumns[$colunmKey]] = $data;
             }
         }
-
+        $this->queryColumns = [];
+        
         return $response;
     }
 
